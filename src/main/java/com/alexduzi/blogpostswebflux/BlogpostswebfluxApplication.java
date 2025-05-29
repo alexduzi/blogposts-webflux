04ce35c4858f9928e5a0e0fc23e33f293e7cf087
alexduzi <duzihd@gmail.com>
2025-05-29 14:54:04 -0300

    first commit

---
+++ src/main/java/com/alexduzi/blogpostswebflux/BlogpostswebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.alexduzi.blogpostswebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostswebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostswebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexduzi/blogpostswebflux/BlogpostswebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexduzi.blogpostswebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostswebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
