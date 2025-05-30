a5ce518712fabece364c44d89c783d13473608ff
alexduzi <duzihd@gmail.com>
2025-05-30 17:55:42 -0300

    posts controller, findall findbyid

--- src/main/java/com/alexduzi/blogpostswebflux/controllers/PostController.java
+++ src/main/java/com/alexduzi/blogpostswebflux/controllers/PostController.java
@@ -1,4 +1,32 @@
 package com.alexduzi.blogpostswebflux.controllers;
 
+import com.alexduzi.blogpostswebflux.models.dto.PostDTO;
+import com.alexduzi.blogpostswebflux.services.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(value = "/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public Flux<PostDTO> findAll() {
+        return postService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Mono<ResponseEntity<PostDTO>> findById(@PathVariable String id) {
+        return postService.findById(id).map(ResponseEntity::ok);
+    }
 }
--- src/main/java/com/alexduzi/blogpostswebflux/services/PostService.java
+++ src/main/java/com/alexduzi/blogpostswebflux/services/PostService.java
@@ -1,7 +1,11 @@
 package com.alexduzi.blogpostswebflux.services;
 
+import com.alexduzi.blogpostswebflux.exceptions.ResourceNotFoundException;
+import com.alexduzi.blogpostswebflux.models.dto.PostDTO;
 import com.alexduzi.blogpostswebflux.repositories.PostRepository;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PostService {
@@ -10,5 +14,15 @@ public class PostService {
 
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    public Flux<PostDTO> findAll() {
+        return postRepository.findAll().map(PostDTO::new);
+    }
+
+    public Mono<PostDTO> findById(String id) {
+        return postRepository.findById(id)
+                .map(PostDTO::new)
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("Post not found!")));
     }
 }
